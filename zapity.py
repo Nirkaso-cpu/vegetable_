735a37d84302988564876c0d3ff85d8dcf0601f1
Nirkaso-cpu <hiroma097@gmail.com>
2025-07-05 11:34:39 +0300

    Update zapity.py

--- zapity.py
+++ zapity.py
@@ -12,37 +12,37 @@ def execute_query(query, params=()):
     except Exception as e:
         print(f"Помилка при виконанні коду: {e}")
 
-# print("\nВся інформація про овоч")
+print("\nВся інформація про овоч")
 
-# execute_query("""
-#         SELECT vegetable_id, vegetable_name, vegetable_price, vegetable_harvest
-#         FROM vegetables
-# """)
+execute_query("""
+        SELECT vegetable_id, vegetable_name, vegetable_price, vegetable_harvest
+        FROM vegetables
+""")
 
-# print("\nЯке виробництво завезе овочі і коли овочі завезуть в магазини")
+print("\nЯке виробництво завезе овочі і коли овочі завезуть в магазини")
 
-# execute_query("""
-#         SELECT production_name, production_delivery
-#         FROM production      
-# """)
+execute_query("""
+        SELECT production_name, production_delivery
+        FROM production      
+""")
 
-# print("\nВиробництва з найбільшою популярністю")
+print("\nВиробництва з найбільшою популярністю")
 
-# execute_query("""
-#         SELECT production_name, production_popularity
-#         FROM production
-#         WHERE production_popularity = 'Висока'     
-# """)
+execute_query("""
+        SELECT production_name, production_popularity
+        FROM production
+        WHERE production_popularity = 'Висока'     
+""")
 
-# print("\nНазва виробництва в алфавітному порядку")
+print("\nНазва виробництва в алфавітному порядку")
 
-# execute_query("""
-#         SELECT production_name
-#         FROM production
-#         ORDER BY production_name ASC                         
-# """)
+execute_query("""
+        SELECT production_name
+        FROM production
+        ORDER BY production_name ASC                         
+""")
 
-print("\nОвочі з ціною вище 20 ")
+print("\nОвочі з ціною вище 20 ₴")
 
 execute_query("""
         SELECT vegetable_name, vegetable_price
@@ -50,10 +50,11 @@ execute_query("""
         WHERE vegetable_price > 20.0
 """)
 
-# print("\nОвочі, зібрані у серпні 2024 року")
+print("\nОвочі, зібрані у серпні 2024 року")
 
-# execute_query("""
-#         SELECT vegetable_name, vegetable_harvest
-#         FROM vegetables
-#         WHERE vegetable_harvest LIKE '2024-08-%'
-# """)
+execute_query("""
+        SELECT vegetable_name, vegetable_harvest
+        FROM vegetables
+        WHERE vegetable_harvest LIKE '2024-08-%'
+""")
+
